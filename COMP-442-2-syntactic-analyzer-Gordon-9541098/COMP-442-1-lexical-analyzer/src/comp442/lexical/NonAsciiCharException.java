b1357c05a3fefaf81d531a5adf8d5774907d3733
Gordon <gb@gordonbailey.net>
2015-04-05 17:01:34 -0400

    Moved everything into COMP-442 and renamed proj

--- COMP-442-2-syntactic-analyzer-Gordon-9541098/COMP-442-1-lexical-analyzer/src/comp442/lexical/NonAsciiCharException.java
+++
@@ -1,9 +0,0 @@
-package comp442.lexical;
-
-import java.io.IOException;
-
-public class NonAsciiCharException extends IOException {
-
-	private static final long serialVersionUID = 1765245913768885820L;
-
-}
